7d69dc03ff6d4a72f71b322188bcd2aa0aef4975
Vishwanath Kannan <vishwanathkannanbsb@gmail.com>
2023-05-27 11:01:47 +0530

    Static url redefined

--- SolverML/settings.py
+++ SolverML/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT  =   os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra lookup directories for collectstatic to find static files
