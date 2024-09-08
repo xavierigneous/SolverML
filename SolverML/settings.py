10a3f2f76e90eba8c38faa640a87ee1509cf5c35
Vishwanath Kannan <vishwanathkannanbsb@gmail.com>
2024-09-08 16:41:09 +0530

    Revert "Updated environment variables"
    This reverts commit 393d70379b1a76257d4af7286d842b10fa4af2e5.

--- SolverML/settings.py
+++ SolverML/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '0p2m+r7+h2c_hipu)s*$z(!l50^=3j%@2!$e9brh8%ar58jyq3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', f"https://{os.environ.get('RAILWAY_PUBLIC_DOMAIN')}"]
+ALLOWED_HOSTS = ['127.0.0.1', os.environ.get('RAILWAY_PUBLIC_DOMAIN')]
 # ALLOWED_HOSTS = ['*', 'https://*.railway.app']
 
 CSRF_TRUSTED_ORIGINS = ['https://127.0.0.1', os.environ.get('RAILWAY_PUBLIC_DOMAIN')]
